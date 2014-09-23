4db69b6cbd1d4c7b36b150ac226194d4a5b09bba
welenton <welenton.w@hotmail.com>
2014-09-23 19:45:51 -0300

    Adicionado modelo Apostador

--- ProjetoBolao/src/java/modelo/GrupoDeApostadores.java
+++ ProjetoBolao/src/java/modelo/GrupoDeApostadores.java
@@ -1,7 +1,21 @@
 package modelo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "GrupoDeApostadores")
 public class GrupoDeApostadores {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "codigo")
+    int codigo;
+
+    @Column(name = "nome", length = 50)
     private String nome;
 
     public String getNome() {
