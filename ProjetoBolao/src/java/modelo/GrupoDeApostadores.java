ebc6e62ff5b4accc491ce749eb325ac9af74fb1c
welenton <welenton.w@hotmail.com>
2014-09-23 19:31:04 -0300

    Adicionado modelo de GrupoDeApostadores

---
+++ ProjetoBolao/src/java/modelo/GrupoDeApostadores.java
@@ -0,0 +1,14 @@
+package modelo;
+
+public class GrupoDeApostadores {
+
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
